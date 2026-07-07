5ff0a1fe13b9bf5be126f9ae3b99fe624f503049
cheedalla pavan pratheek <pratheekcheedalla2005@gmail.com>
2026-07-07 22:53:27 +0530

    Add files via upload

---
+++ Week3/Spring Core – Load Country from Spring Configuration XML/src/test/java/org/example/spring_excercise/SpringExcerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spring_excercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExcerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
